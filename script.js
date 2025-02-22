462840fbc516e87b7eff01477cd35b46addbc48d
susmitamenka <susmitaprasad73@gmail.com>
2025-02-22 17:11:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	// Get form reference
+        const form = document.getElementById('form1');
+
+        // Add event listener to the form
+       
+
+        // Function to retrieve form values and show alert
+        // function getFormvalue(event) {
+            // Prevent the default form submission behavior
+            event.preventDefault();
+
+            // Retrieve input values
+            const firstName = document.querySelector('input[name="fname"]').value;
+            const lastName = document.querySelector('input[name="lname"]').value;
+             form.addEventListener('submit',()=>{
+								     alert(${firstName} ${lastName})};
+            // Display the full name using alert()
+            // alert(${firstName} ${lastName});
+        
 
 }
