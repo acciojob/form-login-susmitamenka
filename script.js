52ae26b68697dcc0537cedf6806d2ffb4823ef3b
susmitamenka <susmitaprasad73@gmail.com>
2025-02-24 14:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
+let form = document.getElementById("form1")
 function getFormvalue() {
     //Write your code here
-	// Get form reference
-        const form = document.getElementById('form1');
-
-            const firstName = document.getElementByid('firstName');
-            const lastName = document.getElementByid('lastName');
-             form.addEventListener("submit",(e)=>{
-				  e.preventDefault()
-				alert(${firstName} ${lastName})};
-            // Display the full name using alert()
-            // alert(${firstName} ${lastName});
+        alert(form.fname.value+" "+form.lname.value)
         
 
 }
